1610a86be3b1f621630eb64af613a24091257d6a
avinashkanaparthy <32629610+avinashkanaparthy@users.noreply.github.com>
2019-04-03 12:25:49 +0530

    Create settings.kts

---
+++ .teamcity/settings.kts
@@ -0,0 +1,26 @@
+package AppTest.buildTypes
+
+import jetbrains.buildServer.configs.kotlin.v2018_2.*
+import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.dotnetBuild
+import jetbrains.buildServer.configs.kotlin.v2018_2.triggers.vcs
+
+object AppTest_Build : BuildType({
+    id("Build")
+    name = "Build"
+
+    vcs {
+        root(random)
+    }
+
+    steps {
+        dotnetBuild {
+            name = "Build .net step"
+            param("dotNetCoverage.dotCover.home.path", "%teamcity.tool.JetBrains.dotCover.CommandLineTools.DEFAULT%")
+        }
+    }
+
+    triggers {
+        vcs {
+        }
+    }
+})
